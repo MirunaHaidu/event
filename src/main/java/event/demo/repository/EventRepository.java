5c534e2c21ab771ca05cc7db3da6056815e22f34
Miruna <miruna_haidu@yahoo.com>
2023-02-18 16:15:45 +0200

    database

--- src/main/java/event/demo/repository/EventRepository.java
+++ src/main/java/event/demo/repository/EventRepository.java
@@ -1,12 +1,13 @@
 package event.demo.repository;
 
 import event.demo.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface EventRepository extends CrudRepository<Event,Integer> {
+public interface EventRepository extends JpaRepository<Event,Integer> {
 Optional<Event> findByTitle(String title);
 }
